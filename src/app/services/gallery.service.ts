2a54a94a13b15d785ea86bbaceec9aa2311a3fc6
Rana Ankit Singh <38341934+Rana-Ankit14@users.noreply.github.com>
2020-07-23 19:11:14 +0530

    Update api url

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -10,6 +10,6 @@ export class GalleryService {
   constructor(private http:HttpClient) { }
 
   getGallery():Observable<any>{
-    return this.http.get<any>('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=2e57eac93f1cd79bfa7d13c5fba36c6d&gallery_id=66911286-72157656404563712&per_page=30&format=json&nojsoncallback=1&auth_token=72157715205700067-0c72fbcd3c178eea&api_sig=38d75f2034b82982aa0b86822ff6236a');
+    return this.http.get<any>('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2e57eac93f1cd79bfa7d13c5fba36c6d&tags=food&per_page=31&page=&format=json&nojsoncallback=1&auth_token=72157715205700067-0c72fbcd3c178eea&api_sig=460766514ae8e5ee4fe5fcd72afc3349');
   }
 }
