c7fdfcdc3f346da470309a93d9ecc4b26a6292f6
Rana Ankit Singh <38341934+Rana-Ankit14@users.noreply.github.com>
2020-07-31 19:21:33 +0530

    Update for Heroku deployment

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/flickr-image-rating'));
+app.use(express.static(__dirname + '/dist/pic'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/flickr-image-rating/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/pic/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000);
